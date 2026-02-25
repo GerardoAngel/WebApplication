86bc55c16d758237304c9687f0131cce341fed27
GerardoAngel <gerardoangel@ciencias.unam.mx>
2026-02-25 12:30:49 -0600

    Practica2_Terminada

--- product/src/main/java/com/product/CtrlProduct.java
+++ product/src/main/java/com/product/CtrlProduct.java
@@ -1,15 +1,21 @@
 package com.product;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/helloworld")
 public class CtrlProduct {
     
-    @GetMapping
-    public String helloWorld(){
-        return "Hello World";
+    @GetMapping("/category")
+    public List<Category> getCategories(){
+        List<Category> categories = Arrays.asList(
+            new Category (1, "Lentes","lens"),
+            new Category(2, "Sombreros", "som")
+        );
+
+        return categories;
     }
 }
