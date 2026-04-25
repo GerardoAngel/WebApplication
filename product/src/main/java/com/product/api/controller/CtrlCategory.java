365b00fe1ffd4bd688c0fa995d223840dcf9844a
GerardoAngel <gerardoangel@ciencias.unam.mx>
2026-04-24 20:07:07 -0600

    Practica8 swagger

--- product/src/main/java/com/product/api/controller/CtrlCategory.java
+++ product/src/main/java/com/product/api/controller/CtrlCategory.java
@@ -16,43 +16,52 @@ import com.product.api.dto.in.DtoCategoryIn;
 import com.product.api.entity.Category;
 import com.product.api.service.SvcCategory;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/category")
+@Tag(name = "Category", description = "Categorías de los productos")
 public class CtrlCategory {
     @Autowired
     SvcCategory svc;
 
     @GetMapping
+    @Operation(summary = "Consultar categorías", description = "Lista las categorías registradas en el sistema")
     public ResponseEntity<List<Category>> findAll() {
         return ResponseEntity.ok(svc.findAll());
     }
 
     @GetMapping("/active")
+    @Operation(summary = "Consultar categorías activas", description = "Lista las categorías activas registradas en el sistema")
     public ResponseEntity<List<Category>> findActive() {
         return ResponseEntity.ok(svc.findActive());
     }
 
     @PostMapping
+    @Operation(summary = "Crear categoría", description = "Crea una nueva categoría en el sistema")
     public ResponseEntity<Void> create(@Valid @RequestBody DtoCategoryIn in) {
     	svc.create(in);
     	return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar categoría", description= "Actualiza los datos de una categoría existente en el sistema")
     public ResponseEntity<Void> update(@PathVariable("id") Integer id, @Valid @RequestBody DtoCategoryIn in) {
     	svc.update(in, id);
     	return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{id}/enable")
+    @Operation(summary = "Habilitar categoría", description = "Habilita una categoría existente en el sistema")
     public ResponseEntity<Void> enable(@PathVariable Integer id) {
     	svc.enable(id);
     	return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{id}/disable")
+    @Operation(summary = "Deshabilitar categoría", description = "Deshabilita una categoría existente en el sistema")
     public ResponseEntity<Void> disable(@PathVariable Integer id) {
     	svc.disable(id);
     	return ResponseEntity.ok().build();
--- product/src/main/java/com/product/api/controller/CtrlProductImage.java
+++ product/src/main/java/com/product/api/controller/CtrlProductImage.java
@@ -14,21 +14,26 @@ import com.product.api.dto.in.DtoProductImageIn;
 import com.product.api.entity.ProductImage;
 import com.product.api.service.SvcProductImage;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/product")
+@Tag(name = "ProductImage", description = "Carga imagenes de los productos")
 public class CtrlProductImage {
 
     @Autowired
     SvcProductImage svc;
 
     @GetMapping("/{id}/image")
+    @Operation(summary = "Consultar imágenes de producto", description = "Lista las imágenes de un producto registrado en el sistema")
     public ResponseEntity<ProductImage> getProductImages(@PathVariable Integer id) {
         return ResponseEntity.ok(svc.getProductImages(id));
     }
 
     @PostMapping("/{id}/image")
+    @Operation(summary = "Crear imagen de producto", description = "Crea una nueva imagen para un producto existente en el sistema")
     public ResponseEntity<String> createProductImage(
             @PathVariable Integer id,
             @Valid @RequestBody DtoProductImageIn in) {
@@ -36,6 +41,7 @@ public class CtrlProductImage {
     }
 
     @DeleteMapping("/{id}/image/{product-image-id}")
+    @Operation(summary = "Eliminar imagen de producto", description = "Elimina una imagen de un producto existente en el sistema")
     public ResponseEntity<String> deleteProductImage(
             @PathVariable("product-image-id") Integer productImageId) {
         return ResponseEntity.ok(svc.deleteProductImage(productImageId));
